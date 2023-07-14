79d56ea79daa33c7ace0e9098d1e50aaba46d9e9
Juhong <juhong@rdv.ai>
2023-07-14 16:49:27 +0900

    여러 bag파일 parsing, 결과를 정리하기 위해 이름으로 폴더 생성

--- main.cpp
+++ main.cpp
@@ -121,195 +121,223 @@ int main(int argc, char **argv)
 			vec_str_path_src.push_back(str_path_src) ;
 		}
 	}
-	    
-	//bag파일 읽기
-	rosbag::Bag bag;
-	bag.open(str_path_src, rosbag::bagmode::Read);  // BagMode is Read by default
-	printf("read : %s\n", str_path_src.c_str()) ;
-	
-	//bag파일의 토픽리스트 얻기
-	rosbag::View view(bag);
-	std::vector<const rosbag::ConnectionInfo *> connection_infos = view.getConnections();
-	std::vector<std::string> topic_list;
-	
-	BOOST_FOREACH(const rosbag::ConnectionInfo *info, connection_infos) 
-	{
-	  topic_list.push_back(info->topic);
-	}
 
-	//토픽 리스트 출력
-	int size_topic_list = topic_list.size() ;
-	printf("\n\nChecked %d topics from %s file ----- \n", size_topic_list, str_path_src.c_str()) ;
-	for( int i=0 ; i<size_topic_list ; i++ )
+	int size_bags = vec_str_path_src.size() ;
+	
+	printf("\n\nBag file list -----\n") ;
+	printf("count = %d\n", size_bags ) ;
+	for( int i=0 ; i<size_bags ; i++ )
 	{
-		printf("Topic : %s\n", topic_list[i].c_str()) ;
+		printf(" - [%d] %s\n", i, vec_str_path_src[i].c_str() ) ;
 	}
 	printf("-----\n") ;
 
-	//bag파일의 timestamp정보
-	ros::Time btime = view.getBeginTime();
-    ros::Time etime = view.getEndTime();
-
-	std::string str_btime = std::to_string(btime.sec) + "." + std::to_string(btime.nsec) ;
-	std::string str_etime = std::to_string(etime.sec) + "." + std::to_string(etime.nsec) ;
-	double dbl_btime = std::stod(str_btime) ;
-	double dbl_etime = std::stod(str_etime) ;
-	
-	printf("\n\nTime Stamp -----\n") ;
-	printf("start time : %s(%f)\n", str_btime.c_str(), dbl_btime) ;
-	printf("end time : %s(%f)\n", str_etime.c_str(), dbl_etime) ;
-	printf("-----\n") ;
-	
-	//ros::Time stamp = ros::Time::now();
-	//std::stringstream ss;
-	//ss << stamp.sec << "." << stamp.nsec;
-	//std::cout << ss.str() << std::endl;
-
-
-	//기본 파일 정보 
-	int ifind = str_path_src.rfind('/') ;
-	std::string str_base_info ;
-	if( ifind >= 0 )
+	for( int i=0 ; i<size_bags ; i++ )
 	{
-		int len = str_path_src.size() - ifind ;
-		str_base_info = str_path_src.substr(ifind+1, len) ;
-	}	
-	printf("bag file name : %s\n", str_base_info.c_str()) ;
-
-	unsigned int data_length_by_sec = 0 ;
-	if( !str_base_info.empty() )
-	{
-		std::string str_only_file_name = boost::filesystem::change_extension(str_base_info, "").string();
-		printf("base info : %s\n", str_only_file_name.c_str()) ;
-
-		//총 몇초짜리지??
-		data_length_by_sec = (unsigned int)(dbl_etime - dbl_btime) ;
-		printf(" - Total %d sec.\n", data_length_by_sec) ;	
+		std::string str_bag_file = vec_str_path_src[i] ;
 		
-		//파일명에서 정보 확인
-		FileInfo fileinfo ;
-		std::vector<std::string> vec_str_base_info = Split(str_only_file_name, '_') ;
-		int size_str_base_info = vec_str_base_info.size() ;
-		if( size_str_base_info > 0 )
-		{
-			if( size_str_base_info > 0)	fileinfo.str_device_name = vec_str_base_info[0] ;
-			if( size_str_base_info > 1)	fileinfo.str_date = vec_str_base_info[1] ;
-			if( size_str_base_info > 2)	fileinfo.str_time = vec_str_base_info[2] ;
-					
-			for( int i=0 ; i<size_str_base_info ; i++ )
-			{
-				printf(" - [%d] %s\n", i, vec_str_base_info[i].c_str()) ;	
-			}
+		printf("Parsing : %s\n", str_bag_file.c_str() ) ;
 
-			printf("\n\n") ;	
-		}		
-
-		//저장하고자 하는 토픽 리스트
-		std::string rgb_cam_image = "/arena_camera_node/image_raw";
-		std::string nir_cam_image = "/arena_camera_node_2/image_raw";
-		std::string stereo_cam_left_image = "/zed2i/zed_node/left/image_rect_color";
-		std::string stereo_cam_right_image = "/zed2i/zed_node/right/image_rect_color"; 
-
-		unsigned int count_rgb_cam_image = 0;
-		unsigned int count_nir_cam_image = 0;
-		unsigned int count_stereo_cam_left_image = 0;
-		unsigned int count_stereo_cam_right_image = 0; 
+		//bag파일 읽기
+		rosbag::Bag bag;
+		bag.open(str_bag_file, rosbag::bagmode::Read);  // BagMode is Read by default
+		printf("read : %s\n", str_bag_file.c_str()) ;
 		
-		std::vector<std::pair<std::string, double>> vec_topic;
-		vec_topic.push_back(std::make_pair(rgb_cam_image, dbl_btime)) ;
-		vec_topic.push_back(std::make_pair(nir_cam_image, dbl_btime)) ;
-		vec_topic.push_back(std::make_pair(stereo_cam_left_image, dbl_btime)) ;
-		vec_topic.push_back(std::make_pair(stereo_cam_right_image, dbl_btime)) ;
-
-		std::vector<unsigned int> vec_topic_count;
-		vec_topic_count.resize(vec_topic.size()) ;
-
-		for(rosbag::MessageInstance const m: rosbag::View(bag))
+		//bag파일의 토픽리스트 얻기
+		rosbag::View view(bag);
+		std::vector<const rosbag::ConnectionInfo *> connection_infos = view.getConnections();
+		std::vector<std::string> topic_list;
+		
+		BOOST_FOREACH(const rosbag::ConnectionInfo *info, connection_infos) 
 		{
-			//현재 ros timestamp 읽기
-			ros::Time mytime = m.getTime() ;
-			std::string str_mytime = std::to_string(mytime.sec) + "." + std::to_string(mytime.nsec) ;
-			double dbl_mytime = std::stod(str_mytime) ;
-			
-			std::string str_topic_name = m.getTopic();			
-			std::vector<std::pair<std::string, double>>::iterator it = find_if(vec_topic.begin(), vec_topic.end(), [&str_topic_name](const std::pair<std::string, double>& elem){ return elem.first == str_topic_name; });
-			if (it != vec_topic.end())
-			{				
-				std::string str_sensor_name ;
-				std::size_t found = str_topic_name.find("arena_camera_node/");
-				if (found!=std::string::npos)
-				{
-					str_sensor_name = "rgb" ;
-					count_rgb_cam_image++ ;
-				}
-				
-				found = str_topic_name.find("arena_camera_node_2/");
-				if (found!=std::string::npos)
-				{
-					str_sensor_name = "nir" ;
-					count_nir_cam_image++ ;
-				}
-
-				found = str_topic_name.find("zed_node/left/");
-				if (found!=std::string::npos)
-				{
-					str_sensor_name = "left" ;
-					count_stereo_cam_left_image++ ;
-				}
-
-				found = str_topic_name.find("zed_node/right/");
-				if (found!=std::string::npos)
-				{
-					str_sensor_name = "right" ;
-					count_stereo_cam_right_image++ ;
-				}
-				
-				//내가 저장했던 시간과 현재 시간 간격을 체크
-				double dbl_save_time_duration = dbl_mytime - it->second ;
-
-				//그 간격이 사용자가 설정한 hz의 duration보다 크다면 저장한다. 
-				if( dbl_save_time_duration >= duration )
-				{
-					//시간 간격을 체크하기 위한 저장 시간 저장
-					it->second = dbl_mytime ;
-	
-					std::string str_write_path = str_path_dst + "/" + str_only_file_name + "_" + str_sensor_name + "_" + std::to_string(dbl_mytime) + ".png" ;
-
-					sensor_msgs::Image::ConstPtr image_ptr = m.instantiate<sensor_msgs::Image>() ;
-					
-					Image_File_Write(image_ptr, str_write_path);
-				}
-
-			}
+		  topic_list.push_back(info->topic);
 		}
 
-		//csv파일 저장
-		std::string str_haead = std::string("DAQ Number, 장소, 날짜, 시간, 센서 이름, 수집 수량, 학습유효 수량");
-		std::string str_rgb_info = fileinfo.str_device_name + "," + "" + "," + fileinfo.str_date + "," + fileinfo.str_time + "," + "rgb" + "," + std::to_string(count_rgb_cam_image) + "," + std::to_string(data_length_by_sec) ;
-		std::string str_nir_info = fileinfo.str_device_name + "," + "" + "," + fileinfo.str_date + "," + fileinfo.str_time + "," + "nir" + "," + std::to_string(count_rgb_cam_image) + "," + std::to_string(data_length_by_sec) ;
-		std::string str_left_info = fileinfo.str_device_name + "," + "" + "," + fileinfo.str_date + "," + fileinfo.str_time + "," + "stereo left" + "," + std::to_string(count_rgb_cam_image) + "," + std::to_string(data_length_by_sec) ;
-		std::string str_right_info = fileinfo.str_device_name + "," + "" + "," + fileinfo.str_date + "," + fileinfo.str_time + "," + "stereo right" + "," + std::to_string(count_rgb_cam_image) + "," + std::to_string(data_length_by_sec) ;
+		//토픽 리스트 출력
+		int size_topic_list = topic_list.size() ;
+		printf("\n\nChecked %d topics from %s file ----- \n", size_topic_list, str_bag_file.c_str()) ;
+		for( int i=0 ; i<size_topic_list ; i++ )
+		{
+			printf("Topic : %s\n", topic_list[i].c_str()) ;
+		}
+		printf("-----\n") ;
+
+		//bag파일의 timestamp정보
+		ros::Time btime = view.getBeginTime();
+	    ros::Time etime = view.getEndTime();
+
+		std::string str_btime = std::to_string(btime.sec) + "." + std::to_string(btime.nsec) ;
+		std::string str_etime = std::to_string(etime.sec) + "." + std::to_string(etime.nsec) ;
+		double dbl_btime = std::stod(str_btime) ;
+		double dbl_etime = std::stod(str_etime) ;
 		
+		printf("\n\nTime Stamp -----\n") ;
+		printf("start time : %s(%f)\n", str_btime.c_str(), dbl_btime) ;
+		printf("end time : %s(%f)\n", str_etime.c_str(), dbl_etime) ;
+		printf("-----\n") ;
+		
+		//ros::Time stamp = ros::Time::now();
+		//std::stringstream ss;
+		//ss << stamp.sec << "." << stamp.nsec;
+		//std::cout << ss.str() << std::endl;
+
+
+		//기본 파일 정보 
+		int ifind = str_bag_file.rfind('/') ;
+		std::string str_base_info ;
+		if( ifind >= 0 )
+		{
+			int len = str_bag_file.size() - ifind ;
+			str_base_info = str_bag_file.substr(ifind+1, len) ;
+		}	
+		printf("bag file name : %s\n", str_base_info.c_str()) ;
+
+		unsigned int data_length_by_sec = 0 ;
+		if( !str_base_info.empty() )
+		{
+			std::string str_only_file_name = boost::filesystem::change_extension(str_base_info, "").string();
+			printf("base info : %s\n", str_only_file_name.c_str()) ;
+
+			//폴더 생성
+			std::string str_write_base_path = str_path_dst + "/" + str_only_file_name ;
+			if(!boost::filesystem::exists(str_write_base_path))
+			{
+				//Create Directory
+				boost::filesystem::path directory(str_write_base_path);
+				boost::filesystem::create_directory(directory);
+			}
+
+			//총 몇초짜리지??
+			data_length_by_sec = (unsigned int)(dbl_etime - dbl_btime) ;
+			printf(" - Total %d sec.\n", data_length_by_sec) ;	
 			
-		std::ofstream outfile;
-		std::string str_csv_write_path = str_path_dst + "/" + str_only_file_name + ".csv" ;
-		outfile.open (str_csv_write_path);
-		outfile << boost::locale::conv::between(str_haead, "EUC-KR", "UTF-8") << std::endl ;
-		outfile << boost::locale::conv::between(str_rgb_info, "EUC-KR", "UTF-8") << std::endl ;
-		outfile << boost::locale::conv::between(str_nir_info, "EUC-KR", "UTF-8") << std::endl ;
-		outfile << boost::locale::conv::between(str_left_info, "EUC-KR", "UTF-8") << std::endl ;
-		outfile << boost::locale::conv::between(str_right_info, "EUC-KR", "UTF-8") << std::endl ;
-		outfile.close();
+			//파일명에서 정보 확인
+			FileInfo fileinfo ;
+			std::vector<std::string> vec_str_base_info = Split(str_only_file_name, '_') ;
+			int size_str_base_info = vec_str_base_info.size() ;
+			if( size_str_base_info > 0 )
+			{
+				if( size_str_base_info > 0)	fileinfo.str_device_name = vec_str_base_info[0] ;
+				if( size_str_base_info > 1)	fileinfo.str_date = vec_str_base_info[1] ;
+				if( size_str_base_info > 2)	fileinfo.str_time = vec_str_base_info[2] ;
+						
+				for( int i=0 ; i<size_str_base_info ; i++ )
+				{
+					printf(" - [%d] %s\n", i, vec_str_base_info[i].c_str()) ;	
+				}
 
-		printf("Write CSV File = %s\n\n\n", str_csv_write_path.c_str()) ;
+				printf("\n\n") ;	
+			}		
 
-		printf("EoF\n") ;
-		printf("\n\nComplete -----\n") ;
-		printf("hm... Ctrl + C\n\n\n\n") ;
+			//저장하고자 하는 토픽 리스트
+			std::string rgb_cam_image = "/arena_camera_node/image_raw";
+			std::string nir_cam_image = "/arena_camera_node_2/image_raw";
+			std::string stereo_cam_left_image = "/zed2i/zed_node/left/image_rect_color";
+			std::string stereo_cam_right_image = "/zed2i/zed_node/right/image_rect_color"; 
+
+			unsigned int count_rgb_cam_image = 0;
+			unsigned int count_nir_cam_image = 0;
+			unsigned int count_stereo_cam_left_image = 0;
+			unsigned int count_stereo_cam_right_image = 0; 
+			
+			std::vector<std::pair<std::string, double>> vec_topic;
+			vec_topic.push_back(std::make_pair(rgb_cam_image, dbl_btime)) ;
+			vec_topic.push_back(std::make_pair(nir_cam_image, dbl_btime)) ;
+			vec_topic.push_back(std::make_pair(stereo_cam_left_image, dbl_btime)) ;
+			vec_topic.push_back(std::make_pair(stereo_cam_right_image, dbl_btime)) ;
+
+			std::vector<unsigned int> vec_topic_count;
+			vec_topic_count.resize(vec_topic.size()) ;
+
+			for(rosbag::MessageInstance const m: rosbag::View(bag))
+			{
+				//현재 ros timestamp 읽기
+				ros::Time mytime = m.getTime() ;
+				std::string str_mytime = std::to_string(mytime.sec) + "." + std::to_string(mytime.nsec) ;
+				double dbl_mytime = std::stod(str_mytime) ;
+				
+				std::string str_topic_name = m.getTopic();			
+				std::vector<std::pair<std::string, double>>::iterator it = find_if(vec_topic.begin(), vec_topic.end(), [&str_topic_name](const std::pair<std::string, double>& elem){ return elem.first == str_topic_name; });
+				if (it != vec_topic.end())
+				{				
+					std::string str_sensor_name ;
+					std::size_t found = str_topic_name.find("arena_camera_node/");
+					if (found!=std::string::npos)
+					{
+						str_sensor_name = "rgb" ;
+						count_rgb_cam_image++ ;
+					}
+					
+					found = str_topic_name.find("arena_camera_node_2/");
+					if (found!=std::string::npos)
+					{
+						str_sensor_name = "nir" ;
+						count_nir_cam_image++ ;
+					}
+
+					found = str_topic_name.find("zed_node/left/");
+					if (found!=std::string::npos)
+					{
+						str_sensor_name = "left" ;
+						count_stereo_cam_left_image++ ;
+					}
+
+					found = str_topic_name.find("zed_node/right/");
+					if (found!=std::string::npos)
+					{
+						str_sensor_name = "right" ;
+						count_stereo_cam_right_image++ ;
+					}
+					
+					//내가 저장했던 시간과 현재 시간 간격을 체크
+					double dbl_save_time_duration = dbl_mytime - it->second ;
+
+					//그 간격이 사용자가 설정한 hz의 duration보다 크다면 저장한다. 
+					if( dbl_save_time_duration >= duration )
+					{
+						//시간 간격을 체크하기 위한 저장 시간 저장
+						it->second = dbl_mytime ;
+		
+						std::string str_write_path = str_write_base_path + "/" + str_only_file_name + "_" + str_sensor_name + "_" + std::to_string(dbl_mytime) + ".png" ;
+
+						sensor_msgs::Image::ConstPtr image_ptr = m.instantiate<sensor_msgs::Image>() ;
+						
+						Image_File_Write(image_ptr, str_write_path);
+					}
+
+				}
+			}
+
+			//csv파일 저장
+			std::string str_haead = std::string("DAQ Number, 장소, 날짜, 시간, 센서 이름, 수집 수량, 학습유효 수량");
+			std::string str_rgb_info = fileinfo.str_device_name + "," + "" + "," + fileinfo.str_date + "," + fileinfo.str_time + "," + "rgb" + "," + std::to_string(count_rgb_cam_image) + "," + std::to_string(data_length_by_sec) ;
+			std::string str_nir_info = fileinfo.str_device_name + "," + "" + "," + fileinfo.str_date + "," + fileinfo.str_time + "," + "nir" + "," + std::to_string(count_rgb_cam_image) + "," + std::to_string(data_length_by_sec) ;
+			std::string str_left_info = fileinfo.str_device_name + "," + "" + "," + fileinfo.str_date + "," + fileinfo.str_time + "," + "stereo left" + "," + std::to_string(count_rgb_cam_image) + "," + std::to_string(data_length_by_sec) ;
+			std::string str_right_info = fileinfo.str_device_name + "," + "" + "," + fileinfo.str_date + "," + fileinfo.str_time + "," + "stereo right" + "," + std::to_string(count_rgb_cam_image) + "," + std::to_string(data_length_by_sec) ;
+			
+				
+			std::ofstream outfile;
+			std::string str_csv_write_path = str_write_base_path + "/" + str_only_file_name + ".csv" ;
+			outfile.open (str_csv_write_path);
+			outfile << boost::locale::conv::between(str_haead, "EUC-KR", "UTF-8") << std::endl ;
+			outfile << boost::locale::conv::between(str_rgb_info, "EUC-KR", "UTF-8") << std::endl ;
+			outfile << boost::locale::conv::between(str_nir_info, "EUC-KR", "UTF-8") << std::endl ;
+			outfile << boost::locale::conv::between(str_left_info, "EUC-KR", "UTF-8") << std::endl ;
+			outfile << boost::locale::conv::between(str_right_info, "EUC-KR", "UTF-8") << std::endl ;
+			outfile.close();
+
+			printf("Write CSV File = %s\n\n\n", str_csv_write_path.c_str()) ;
+
+			printf("EoF\n") ;
+			printf("\n\nComplete -----\n") ;
+			printf("hm... Ctrl + C\n\n\n\n") ;
+		}
+
+		bag.close();
+		printf("End : %s\n", str_bag_file.c_str() ) ;
 	}
 
-
-	bag.close();
+	
 	ros::shutdown();
 	
 	return 0 ;
